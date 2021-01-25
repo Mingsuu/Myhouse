c2487fdbeffabf5c354b630f414137b6d710183f
Minsu <alstndkrl@naver.com>
2021-01-25 16:35:28 +0900

    minsu

---
+++ src/main/java/com/myhouse/vo/questionVO.java
@@ -0,0 +1,48 @@
+package com.myhouse.vo;
+
+public class questionVO {
+	String qno,gno,qtype,qcontent,qreply;
+
+	
+	
+	public String getQno() {
+		return qno;
+	}
+
+	public void setQno(String qno) {
+		this.qno = qno;
+	}
+
+	public String getGno() {
+		return gno;
+	}
+
+	public void setGno(String gno) {
+		this.gno = gno;
+	}
+
+	public String getQtype() {
+		return qtype;
+	}
+
+	public void setQtype(String qtype) {
+		this.qtype = qtype;
+	}
+
+	public String getQcontent() {
+		return qcontent;
+	}
+
+	public void setQcontent(String qcontent) {
+		this.qcontent = qcontent;
+	}
+
+	public String getQreply() {
+		return qreply;
+	}
+
+	public void setQreply(String qreply) {
+		this.qreply = qreply;
+	}
+
+}
